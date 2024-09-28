343e53ebff701f812dde09260f03a0b27f68d9a4
azim patel <azimp004@gmail.com>
2024-09-28 13:34:13 +0530

    assignment 6

---
+++ src/main/java/Assignment6/AddNumber.java
@@ -0,0 +1,24 @@
+package Assignment6;
+
+public class AddNumber {
+
+    public AddNumber(int num1,int num2)
+    {
+        System.out.println(num1+num2);
+    }
+
+    public AddNumber(int num1,int num2,int num3)
+    {
+        System.out.println(num1+num2+num3);
+    }
+
+    public AddNumber(double num1,double num2)
+    {
+        System.out.println(num1+num2);
+    }
+
+    public AddNumber(double num1,double num2,double num3)
+    {
+        System.out.println(num1+num2+num3);
+    }
+}
---
+++ src/main/java/Assignment6/AddNumberMain.java
@@ -0,0 +1,15 @@
+package Assignment6;
+
+public class AddNumberMain {
+    public static void main(String[] args) {
+
+        AddNumber add1=new AddNumber(10,5);
+        AddNumber add2=new AddNumber(100,50,300);
+        AddNumber add3=new AddNumber(10.6,50.55);
+        AddNumber add4=new AddNumber(12.4,45.7,20.8);
+
+
+
+
+    }
+}
